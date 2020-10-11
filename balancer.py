fc90b2ee9456a999ed9ae9f657f6e05731e3a6bf
valerijmaculevic <Mama1753516>
2020-10-11 21:37:01 +0300

    memory and speed improvements

--- balancer.py
+++ balancer.py
@@ -3,38 +3,41 @@ from funs import bisect_right, string_generator, print_result, find_gcd
 
 class Balancer:
     def __init__(self, weights_list=[1]):
-        self.weights_list = weights_list
+        self.reset_weights(weights_list)
 
     def reset_weights(self, weights_list=[1]):
-        self.__init__(weights_list)
+        self.weights_list = weights_list
 
     def get_instance_index(self, query):
         pass
 
 
 class SimpleHashing(Balancer):
-    def __init__(self, weights_list=[1]):
+    def reset_weights(self, weights_list=[1]):
         self.weights_list = [x // find_gcd(weights_list) for x in weights_list]
+        self.inst_indexes = []
+        for inst_ind in range(len(self.weights_list)):
+            for n in range(self.weights_list[inst_ind]):
+                self.inst_indexes.append(inst_ind)
 
     def get_instance_index(self, query):
         sub_instance_index = hash(query) % sum(self.weights_list)
-        for instance_index in range(len(self.weights_list)):
-            sub_instance_index -= self.weights_list[instance_index]
-            if sub_instance_index < 0:
-                return instance_index
+        return self.inst_indexes[sub_instance_index]
 
 
 class RendezVousHashing(Balancer):
     def get_instance_index(self, query):
-        sub_instances = []
+        max_hash = None
         for inst_index in range(len(self.weights_list)):
             for sub_inst_index in range(self.weights_list[inst_index]):
-                sub_instances.append((hash((inst_index, sub_inst_index, query)), inst_index))
-        return max(sub_instances)[1]
+                hash_with_index = (hash((inst_index, sub_inst_index, query)), inst_index)
+                if max_hash is None or hash_with_index[0] > max_hash[0]:
+                    max_hash = hash_with_index
+        return max_hash[1]
 
 
 class ConsistentHashing(Balancer):
-    def __init__(self, weights_list=[1]):
+    def reset_weights(self, weights_list=[1]):
         self.weights_list = weights_list
         self.sub_instances = []
         for inst_index in range(len(self.weights_list)):
@@ -69,14 +72,20 @@ class Instance:
         self.cache_hit_number = 0
 
 
-def test(queries_number, weights_lists):
-    queries = [string_generator(5) for n in range(queries_number)]
+def test_balancer(queries, weights_lists, instances, balancer):
+    for weights in weights_lists:
+        balancer.reset_weights(weights)
+        for query in queries:
+            choice = balancer.get_instance_index(query)
+            instances[choice].process_query(query)
 
+
+def test(queries, weights_lists):
     max_len = len(max(weights_lists, key=len))
 
     all_instances = [[Instance() for y in range(max_len)] for x in range(3)]
 
-    relative_weights = [[y / sum(x) * queries_number for y in x] for x in weights_lists]
+    relative_weights = [[y / sum(x) * len(queries) for y in x] for x in weights_lists]
 
     ideal_queries_count = [int(sum([(i[n] if n < len(i) else 0) for i in relative_weights])) for n in range(max_len)]
 
@@ -84,25 +93,16 @@ def test(queries_number, weights_lists):
     consistent_balancer = ConsistentHashing()
     rendezvous_balancer = RendezVousHashing()
 
-    for weights in weights_lists:
-        simple_balancer.reset_weights(weights)
-        consistent_balancer.reset_weights(weights)
-        rendezvous_balancer.reset_weights(weights)
-
-        for query in queries:
-            choice1 = simple_balancer.get_instance_index(query)
-            choice2 = consistent_balancer.get_instance_index(query)
-            choice3 = rendezvous_balancer.get_instance_index(query)
-
-            all_instances[0][choice1].process_query(query)
-            all_instances[1][choice2].process_query(query)
-            all_instances[2][choice3].process_query(query)
+    test_balancer(queries, weights_lists, all_instances[0], simple_balancer)
+    test_balancer(queries, weights_lists, all_instances[1], consistent_balancer)
+    test_balancer(queries, weights_lists, all_instances[2], rendezvous_balancer)
 
     print_result(all_instances, ideal_queries_count)
 
 
-test(10000, [[100, 200, 300, 500], [100, 200, 300]])
+queries = [string_generator(5) for n in range(10000)]
 
+test(queries, [[100, 200, 300, 400], [100, 200, 300]])
 # this test get queryset size and list of lists of weights
 # 1) distributes queries across instances
 # 2) reset weights
